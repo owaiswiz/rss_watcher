baf20be3b9e523837ff707555f0d35cd2258e446
Owais <owaiswiz@gmail.com>
2020-01-18 21:12:00 +0530

    Use Cache-Control headers to disable any caches

--- lib/rss_watcher.rb
+++ lib/rss_watcher.rb
@@ -11,7 +11,7 @@ module RssWatcher
     #updated_interval - Seconds to wait for between checking for updates to the RSS feed
     #cache_file - Path to a file that this gem uses internally for remembering the last seen item
     while true
-      open(url) do |rss|
+      open(url, {"Cache-Control" => "no-cache", "Pragma" => "no-cache"}) do |rss|
         feed = RSS::Parser.parse(rss)
         RssWatcher::FeedManager.new(cache_file).get_new_feed_items(feed.items).each do |item|
           yield item if block_given?
